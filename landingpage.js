1df3123a953ac468d7e8343009c1f64b447292c3
ChristineJoyRemedio <47110612+ChristineJoyRemedio@users.noreply.github.com>
2019-02-13 17:50:05 +0800

    Add files via upload

---
+++ landingpage.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import { Grid, Cell} from 'react-mdl';
+
+
+class LandingPage extends Component {
+  render(){
+    return(
+
+      <div>
+
+      </div>
+    )
+   }
+  }
+
+  export default LandingPage;
